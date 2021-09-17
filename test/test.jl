a7e5fdc5c36aa8b8da024be949b6595e2dfde16c
Takuma Teramura <takuma@amadeus.local>
2021-09-17 22:03:34 +0900

    eV2J

--- test/test.jl
+++ test/test.jl
@@ -3,17 +3,30 @@ using Plots
 # %%
 include("../src/Laser.jl")
 # %%
+function dfdt(f, dt)
+    n = length(f)
+    g = copy(f)
+    g[1] = (f[2] - f[1]) / dt
+    g[n] = (f[n] - f[n - 1]) / dt
+    for i in 2:n - 1
+        g[i] = (f[i + 1] - f[i - 1]) / 2dt
+    end
+    return g
+end
+ # %%
 pulseEt = Laser.Pulse(1, 800, 0, :sin4, 10, :Et)
 pulseAt = Laser.Pulse(1, 800, 0, :sin4, 10, :At)
-pulse = pulseEt
+pulse = pulseAt
 time = Laser.Time(pulse, 1000, 10)
 # %%
-Et = Laser.getEt(time.t, pulseAt)
+Et = Laser.getEt(time.t, pulse)
 # %%
-At = Laser.getAt(time.t, pulseAt)
+At = Laser.getAt(time.t, pulse)
 # %%
-p1 = plot(time.t, Et, label="Et")
+dAdt = dfdt(At, time.dt)
 # %%
+p1 = plot(time.t, Et, label="Et", color=:black, linewidth=3.0)
+p1 = plot!(time.t, -dAdt, label="-dAdt")
 p2 = plot(time.t, At, label="At")
-# %%
 plot(p1, p2, layout=(2, 1))
+# %%
